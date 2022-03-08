67be06fa33dcb3654ee583fde8efe11f59ab6493
ASHISH BAJPAI <ashishvirgo12@gmail.com>
2022-03-08 10:41:20 +0530

    apply try-catch block

--- testEx1.java
+++ testEx1.java
@@ -2,9 +2,15 @@ public class testEx1 {
     public static void main(String arg[]){
         int a=5;
         int b=0;
+        try{
         int c=a/b;
         System.out.println("a="+a);
         System.out.println("b="+b);
         System.out.println("c="+c);
+        }
+        catch(Exception e){
+            e.getStackTrace();
+            System.out.println("Change the value of b"+e);   
+        }
     }
 }
